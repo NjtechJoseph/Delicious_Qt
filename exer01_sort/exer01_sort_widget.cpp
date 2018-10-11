ca5498ce8e4bcae62583df7359b5e5101cf6c40b
NjtechJoseph <zhoujialinjoseph@icloud.com>
2018-10-11 21:53:51 +0800

    基本功能已出，但还需另外修改

--- exer01_sort/exer01_sort_widget.cpp
+++ exer01_sort/exer01_sort_widget.cpp
@@ -6,34 +6,102 @@ exer01_sort_Widget::exer01_sort_Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::exer01_sort_Widget)
 {
-    QVector<QString> luzhishen;
-    QVector<QString> linchong;
-    QVector<QString> songjiang;
-    QVector<QString> wusong;
-    luzhishen.append({"1403130209", "鲁智深",  "80",  "72"});
-    linchong.append({"1403140101",  "林冲",    "82",  "76"});
-    songjiang.append({"1403130102", "宋江",    "76",  "85"});
-    wusong.append({"1403130103",    "武松",    "88",  "80"});
     ui->setupUi(this);
+//    QVector<QVector<QString>> table(4);     //新建的二维向量
+//        sortbylesson1(table);
+//        for(int i=0;i<=3;i++)
+//            for(int j=0;j<=3;j++)
+//            {
+//                ui->tableWidget->setItem(i,j,new QTableWidgetItem(table[i][j]));
+//            }
 
-    //    ui->tableWidget->setItem(0,0,new QTableWidgetItem(luzhishen[1]));    //    void setItem(int row, int column, QTableWidgetItem *item)
-
-    //    qDebug()<<values[0];
-    //    ui->tableWidget->setColumnWidth(0, 150);//will be used later
-    //    ui->tableWidget->setRowHeight(0,50);
-    //    ui->tableWidget->setColumnCount(3);
-    //    ui->tableWidget->setRowCount(5);
-    //    QStringList List;
-    //    List<<"name0"<<"name1"<<"name2";
-    //    ui->tableWidget->setHorizontalHeaderLabels(List);                     //为表格添加列名
-    //    ui->tableWidget->setColumnWidth(0,100);
-    //    ui->tableWidget->setColumnWidth(1,200);
-    //    ui->tableWidget->horizontalHeader()->setStretchLastSection(1);        //将表头的第2列宽度延伸到整个列表全满
 }
 
 exer01_sort_Widget::~exer01_sort_Widget()
 {
     delete ui;
+}
+
+
+
+void exer01_sort_Widget::sortbyname(QVector<QVector<QString>> &table0)
+{
+    table0[1].append({"1403130209", "鲁智深",  "80",  "72"});
+    table0[0].append({"1403140101", "林冲",    "82",  "76"});
+    table0[2].append({"1403130102", "宋江",    "76",  "85"});
+    table0[3].append({"1403130103", "武松",    "88",  "80"});
+}
+
+void exer01_sort_Widget::sortbylesson1(QVector<QVector<QString> > &table0)
+{
+    table0[2].append({"1403130209", "鲁智深",  "80",  "72"});
+    table0[1].append({"1403140101", "林冲",    "82",  "76"});
+    table0[3].append({"1403130102", "宋江",    "76",  "85"});
+    table0[0].append({"1403130103", "武松",    "88",  "80"});
+}
+
+void exer01_sort_Widget::sortbylesson2(QVector<QVector<QString> > &table0)
+{
+    table0[3].append({"1403130209", "鲁智深",  "80",  "72"});
+    table0[2].append({"1403140101", "林冲",    "82",  "76"});
+    table0[0].append({"1403130102", "宋江",    "76",  "85"});
+    table0[1].append({"1403130103", "武松",    "88",  "80"});
+}
+
+
+
+
+
+
+void exer01_sort_Widget::on_pushButton_clicked()
+{
+    QVector<QVector<QString>> table(4);     //新建的二维向量
+        sortbyname(table);
+        for(int i=0;i<=3;i++)
+            for(int j=0;j<=3;j++)
+            {
+                ui->tableWidget->setItem(i,j,new QTableWidgetItem(QString("")));
+            }
+        for(int i=0;i<=3;i++)
+            for(int j=0;j<=3;j++)
+            {
+                ui->tableWidget->setItem(i,j,new QTableWidgetItem(table[i][j]));
+            }
+
+}
+
+void exer01_sort_Widget::on_pushButton_2_clicked()
+{
+    QVector<QVector<QString>> table(4);     //新建的二维向量
+        sortbylesson1(table);
+        for(int i=0;i<=3;i++)
+            for(int j=0;j<=3;j++)
+            {
+                ui->tableWidget->setItem(i,j,new QTableWidgetItem(QString("")));
+            }
+        for(int i=0;i<=3;i++)
+            for(int j=0;j<=3;j++)
+            {
+                ui->tableWidget->setItem(i,j,new QTableWidgetItem(table[i][j]));
+            }
+
+}
+
+void exer01_sort_Widget::on_pushButton_3_clicked()
+{
+    QVector<QVector<QString>> table(4);     //新建的二维向量
+        sortbylesson2(table);
+        for(int i=0;i<=3;i++)
+        for(int j=0;j<=3;j++)
+        {
+            ui->tableWidget->setItem(i,j,new QTableWidgetItem(QString("")));
+        }
+        for(int i=0;i<=3;i++)
+            for(int j=0;j<=3;j++)
+            {
+                ui->tableWidget->setItem(i,j,new QTableWidgetItem(table[i][j]));
+            }
+
 }
 
 
--- exer01_sort/exer01_sort_widget.h
+++ exer01_sort/exer01_sort_widget.h
@@ -2,6 +2,7 @@
 #define EXER01_SORT_WIDGET_H
 
 #include <QWidget>
+#include <qdebug.h>
 
 namespace Ui {
 class exer01_sort_Widget;
@@ -13,9 +14,20 @@ class exer01_sort_Widget : public QWidget
 
 public:
     explicit exer01_sort_Widget(QWidget *parent = 0);
-    ~exer01_sort_Widget();
 
+    void sortbyname(QVector<QVector<QString>> &table0);
+    void sortbylesson1(QVector<QVector<QString>> &table0);
+    void sortbylesson2(QVector<QVector<QString>> &table0);
+
+    ~exer01_sort_Widget();
 private slots:
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
 
 private:
     Ui::exer01_sort_Widget *ui;
